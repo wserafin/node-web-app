82cca06f93021366af5de08b54b1f0b9550b4dcd
Roland Barcia <barcia@us.ibm.com@rb.local>
2020-05-04 09:01:01 -0400

    Change for InnerCircle

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FastStart, new Inner Circle !!!!!!  Latest Yeah ');
+  res.send('Hello Inner Circle !!!!!!!  Latest Yeah ');
 });
 
 app.listen(PORT, HOST);
